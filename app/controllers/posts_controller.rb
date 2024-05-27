aa380a34c8a7d8a1d975375c215fffdef8691b51
kumamoto ryota <kumamotoryota@MacBook-Pro.local>
2024-05-27 14:27:53 +0900

    add_post_update_destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,9 +14,33 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(content: params[:content])
     if @post.save
+      flash[:notice] = "投稿が保存されました"
       redirect_to("/posts/index")
     else
       render("posts/new")
     end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      puts @post.errors.full_messages
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
   end
 end
